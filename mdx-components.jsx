ce7333b26b70e24142e282434871ff931092bded
Noah Sausen <noahsausen@gmail.com>
2026-04-24 00:26:15 -0400

    im going crazy

--- mdx-components.jsx
+++ mdx-components.jsx
@@ -1,9 +1,8 @@
 import { withGitHubAlert, Callout, Blockquote } from 'nextra/components'
-
-const AlertBlockquote = withGitHubAlert(
-  ({ type, children }) => <Callout type={type}>{children}</Callout>,
-  Blockquote
-)
+ 
+const AlertBlockquote = withGitHubAlert(({ type, children }) => {
+  return <Callout type={type}>{children}</Callout>,
+}, Blockquote)
 
 export function useMDXComponents(components) {
   return {
